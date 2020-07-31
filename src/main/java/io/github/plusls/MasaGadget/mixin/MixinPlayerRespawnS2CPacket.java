dc9c3046792d4e5ecfda24029a3b7aacdde8cdae
plusls <plusls@qq.com>
2020-07-31 22:55:05 +0800

    merge from 1.16.x

--- src/main/java/io/github/plusls/MasaGadget/mixin/MixinPlayerRespawnS2CPacket.java
+++ src/main/java/io/github/plusls/MasaGadget/mixin/MixinPlayerRespawnS2CPacket.java
@@ -4,12 +4,7 @@ import fi.dy.masa.minihud.util.DataStorage;
 import io.github.plusls.MasaGadget.util.ParseBborPacket;
 import net.minecraft.network.listener.ClientPlayPacketListener;
 import net.minecraft.network.packet.s2c.play.PlayerRespawnS2CPacket;
-<<<<<<< HEAD
 import net.minecraft.world.dimension.DimensionType;
-=======
-import net.minecraft.util.registry.RegistryKey;
-import net.minecraft.world.World;
->>>>>>> 1.16.x
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -20,13 +15,8 @@ public abstract class MixinPlayerRespawnS2CPacket {
     @Redirect(method = "apply(Lnet/minecraft/network/listener/ClientPlayPacketListener;)V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/network/listener/ClientPlayPacketListener;onPlayerRespawn(Lnet/minecraft/network/packet/s2c/play/PlayerRespawnS2CPacket;)V"))
     void redirectOnPlayerRespawn(ClientPlayPacketListener listener, PlayerRespawnS2CPacket packet) {
-<<<<<<< HEAD
         DimensionType oldDimension = ((IMixinClientPlayNetworkHandler) listener).accessor$getClient().player.dimension;
         DimensionType newDimension = packet.getDimension();
-=======
-        RegistryKey<World> oldDimension = ((IMixinClientPlayNetworkHandler) listener).accessor$getClient().player.world.getRegistryKey();
-        RegistryKey<World> newDimension = packet.getDimension();
->>>>>>> 1.16.x
         listener.onPlayerRespawn(packet);
         if (oldDimension != newDimension) {
             // reload minihud struct when dimension change
