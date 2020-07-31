33889b24ffa7d4126384f2435158c892a50f4ce9
plusls <plusls@qq.com>
2020-07-31 10:22:30 +0800

    update to 1.0.1-alpha.1 make minihud support bbor server

---
+++ src/main/java/io/github/plusls/MasaGadget/mixin/MixinCustomPayloadS2CPacket.java
@@ -0,0 +1,62 @@
+package io.github.plusls.MasaGadget.mixin;
+
+import fi.dy.masa.minihud.util.DataStorage;
+import io.github.plusls.MasaGadget.MasaGadgetMod;
+import io.github.plusls.MasaGadget.util.ParseBborPacket;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.network.Packet;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.listener.ClientPlayPacketListener;
+import net.minecraft.network.packet.s2c.play.CustomPayloadS2CPacket;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CustomPayloadS2CPacket.class)
+public abstract class MixinCustomPayloadS2CPacket implements Packet<ClientPlayPacketListener> {
+    @Shadow
+    private Identifier channel;
+
+    @Inject(method = "apply(Lnet/minecraft/network/listener/ClientPlayPacketListener;)V",
+            at = @At(value = "HEAD"), cancellable = true)
+    private void onApply(ClientPlayPacketListener clientPlayPacketListener, CallbackInfo info) {
+        CustomPayloadS2CPacket packet = (CustomPayloadS2CPacket) (Object)this;
+        String channelName = channel.toString();
+        if (channelName.startsWith("bbor:")) {
+            PacketByteBuf data = null;
+            try {
+                data = packet.getData();
+                switch (channelName) {
+                    case "bbor:initialize": {
+                        if (!MasaGadgetMod.bborCompat) {
+                            long seed = data.readLong();
+                            int spawnX = data.readInt();
+                            int spawnZ = data.readInt();
+                            DataStorage.getInstance().setWorldSeed(seed);
+                            DataStorage.getInstance().setWorldSpawn(new BlockPos(spawnX, 0, spawnZ));
+                            ((ClientPlayNetworkHandler) clientPlayPacketListener).sendPacket(MasaGadgetMod.BBOR_SUBSCRIBE_PACKET);
+                        }
+                        ParseBborPacket.structuresCache = new ListTag();
+                        break;
+                    }
+                    case "bbor:add_bounding_box_v2": {
+                        ParseBborPacket.parse(data);
+                        break;
+                    }
+                }
+            } finally {
+                if (data != null)
+                    data.release();
+            }
+            // 兼容 bbor
+            if (!MasaGadgetMod.bborCompat) {
+                info.cancel();
+            }
+        }
+    }
+}
---
+++ src/main/java/io/github/plusls/MasaGadget/util/BoundingBoxDeserializer.java
@@ -0,0 +1,56 @@
+package io.github.plusls.MasaGadget.util;
+
+import fi.dy.masa.malilib.util.IntBoundingBox;
+import io.github.plusls.MasaGadget.MasaGadgetMod;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.IntArrayTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.network.PacketByteBuf;
+
+public class BoundingBoxDeserializer {
+
+    public static CompoundTag deserializeStructure(PacketByteBuf buf) {
+        CompoundTag tag = new CompoundTag();
+
+        deserializeStructureBox(buf, tag, true);
+        if (!tag.contains("BB")) {
+            return null;
+        }
+
+        ListTag childrenTagList = new ListTag();
+
+        while (buf.isReadable()) {
+            CompoundTag childrenTag = new CompoundTag();
+            deserializeStructureBox(buf, childrenTag, false);
+            childrenTagList.add(childrenTag);
+        }
+
+        tag.put("Children", childrenTagList);
+        return tag;
+    }
+
+    private static void deserializeStructureBox(PacketByteBuf buf, CompoundTag tag, boolean first) {
+        if (!buf.isReadable(2) || buf.readChar() != 'S')
+            return;
+
+        String typeId = ParseBborPacket.bborIdToMinihudId(buf.readInt());
+        if (typeId.equals("")) {
+            return;
+        }
+
+        int minX = buf.readVarInt();
+        int minY = buf.readVarInt();
+        int minZ = buf.readVarInt();
+        int maxX = buf.readVarInt();
+        int maxY = buf.readVarInt();
+        int maxZ = buf.readVarInt();
+        IntArrayTag boundingBox = new IntArrayTag(new int[]{minX, minY, minZ, maxX, maxY, maxZ});
+
+        tag.put("BB", boundingBox);
+
+        if (first) {
+            tag.putString("id", typeId);
+        }
+        // MasaGadgetMod.LOGGER.info(String.format("%s: (%d, %d, %d) (%d, %d, %d)", typeId, minX, minY, minZ, maxX, maxY, maxZ));
+    }
+}
