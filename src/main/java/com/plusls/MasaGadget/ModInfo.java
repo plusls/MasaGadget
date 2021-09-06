9efb64d0542a3c74894f9ba7842fc34bf498b370
Hendrix-Shen <HendrixShen@qq.com>
2021-09-06 22:49:57 +0800

    Fix version

--- src/main/java/com/plusls/MasaGadget/ModInfo.java
+++ src/main/java/com/plusls/MasaGadget/ModInfo.java
@@ -1,5 +1,6 @@
 package com.plusls.MasaGadget;
 
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -7,7 +8,7 @@ import org.apache.logging.log4j.Logger;
 public class ModInfo {
     public static String MOD_ID = "masa_gadget_mod";
     public static final String MOD_NAME = "MasaGadget";
-    public static final String MOD_VERSION = "2.0.0-build.undefined";
+    public static final String MOD_VERSION = FabricLoader.getInstance().getModContainer(MOD_ID).get().getMetadata().getVersion().toString();
     public static final String MOD_VERSION_TYPE = "Version Exception";
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
