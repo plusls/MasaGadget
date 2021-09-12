ab911c8f71fc7eef4310514cdb2c751d6caa0de2
plusls <plusls@qq.com>
2021-09-12 13:55:25 +0800

    add autoSyncTradeOfferList add inventoryPreviewSupportTradeOfferList

--- src/main/java/com/plusls/MasaGadget/mixin/tweakeroo/pcaSyncProtocol/MixinRenderUtils.java
+++ src/main/java/com/plusls/MasaGadget/mixin/tweakeroo/pcaSyncProtocol/MixinRenderUtils.java
@@ -11,7 +11,7 @@ import fi.dy.masa.tweakeroo.renderer.RenderUtils;
 import net.minecraft.block.entity.*;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.HorseBaseEntity;
-import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.entity.passive.MerchantEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.util.hit.EntityHitResult;
@@ -54,7 +54,7 @@ public abstract class MixinRenderUtils {
     private static Entity redirectGetEntity(EntityHitResult entityHitResult) {
         Entity entity = entityHitResult.getEntity();
         if (Configs.Tweakeroo.PCA_SYNC_PROTOCOL.getBooleanValue() && PcaSyncProtocol.enable) {
-            if (entity instanceof Inventory || entity instanceof VillagerEntity || entity instanceof HorseBaseEntity
+            if (entity instanceof Inventory || entity instanceof MerchantEntity || entity instanceof HorseBaseEntity
                     || entity instanceof PlayerEntity) {
                 PcaSyncProtocol.syncEntity(entity.getId());
             }
