1378c40023b29109f486dadf48f0802089713d8c
plusls <plusls@qq.com>
2022-01-19 17:28:35 +0800

    rewrite canSpawn check

--- src/main/java/com/plusls/MasaGadget/util/SearchMobSpawnPointUtil.java
+++ src/main/java/com/plusls/MasaGadget/util/SearchMobSpawnPointUtil.java
@@ -11,14 +11,15 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.EntityType;
-import net.minecraft.text.LiteralText;
-import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.SpawnRestriction;
+import net.minecraft.text.*;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.LightType;
+import net.minecraft.world.SpawnHelper;
 import net.minecraft.world.chunk.WorldChunk;
 import net.minecraft.world.chunk.light.LightingProvider;
 import org.jetbrains.annotations.Nullable;
@@ -66,9 +67,8 @@ public class SearchMobSpawnPointUtil {
         int maxZ = pos.getZ() + 129;
         BlockPos.Mutable currentPos = new BlockPos.Mutable();
         int maxSpawnLightLevel = Configs.Generic.LIGHT_LEVEL_THRESHOLD.getIntegerValue();
-        int searchNum = 0;
         LightingProvider lightingProvider = world.getChunkManager().getLightingProvider();
-
+        EntityType<?> entityType = world.getDimension().isUltrawarm() ? EntityType.ZOMBIFIED_PIGLIN : EntityType.CREEPER;
         for (int x = pos.getX() - 129; x <= maxX; ++x) {
             for (int z = pos.getZ() - 129; z <= maxZ; ++z) {
                 WorldChunk chunk = world.getChunk(x >> 4, z >> 4);
@@ -77,7 +77,6 @@ public class SearchMobSpawnPointUtil {
                 }
                 int maxY = Math.min(pos.getY() + 129, chunk.sampleHeightmap(Heightmap.Type.WORLD_SURFACE, x, z) + 1);
                 for (int y = Math.max(pos.getY() - 129, world.getBottomY() + 1); y <= maxY; ++y) {
-                    ++searchNum;
                     if (squaredDistanceTo(x, y, z, centerPos) > 16384) {
                         if (y > centerPos.y) {
                             break;
@@ -89,11 +88,7 @@ public class SearchMobSpawnPointUtil {
                         continue;
                     }
                     currentPos.set(x, y, z);
-                    BlockState state = world.getBlockState(currentPos);
-                    if (state.getCollisionShape(world, currentPos).isEmpty() &&
-                            !state.isOf(Blocks.REDSTONE_WIRE) &&
-                            state.getFluidState().isEmpty() &&
-                            world.getBlockState(currentPos.down()).allowsSpawning(world, pos, EntityType.CREEPER) &&
+                    if (SpawnHelper.canSpawn(SpawnRestriction.getLocation(entityType), world, pos, entityType) &&
                             lightingProvider.get(LightType.BLOCK).getLightLevel(currentPos) < maxSpawnLightLevel) {
                         spawnPos = currentPos.mutableCopy();
                     }
@@ -102,11 +97,10 @@ public class SearchMobSpawnPointUtil {
         }
         Text text;
         if (spawnPos == null) {
-            text = new LiteralText("No block can spawn!");
+            text = new TranslatableText("masa_gadget_mod.message.noBlockCanSpawn").setStyle(Style.EMPTY.withColor(TextColor.fromFormatting(Formatting.GREEN)));
         } else {
-            text = new LiteralText(String.format("pos: [%d, %d, %d] searchNum: %d", spawnPos.getX(), spawnPos.getY(), spawnPos.getZ(), searchNum));
+            text = new TranslatableText("masa_gadget_mod.message.spawnPos", spawnPos.getX(), spawnPos.getY(), spawnPos.getZ());
             player.sendChatMessage(String.format("/highlightWaypoint %d %d %d", spawnPos.getX(), spawnPos.getY(), spawnPos.getZ()));
-
         }
         Objects.requireNonNull(MinecraftClient.getInstance().player).sendMessage(text, false);
     }
