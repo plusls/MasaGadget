d14731f996b86a15fc1dc4a759ab1cf093316254
plusls <plusls@qq.com>
2022-01-22 17:06:26 +0800

    format code

--- src/main/java/com/plusls/MasaGadget/mixin/minihud/compactBborProtocol/MixinCustomPayloadS2CPacket.java
+++ src/main/java/com/plusls/MasaGadget/mixin/minihud/compactBborProtocol/MixinCustomPayloadS2CPacket.java
@@ -10,7 +10,6 @@ import net.minecraft.network.Packet;
 import net.minecraft.network.listener.ClientPlayPacketListener;
 import net.minecraft.network.packet.s2c.play.CustomPayloadS2CPacket;
 import net.minecraft.util.Identifier;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
