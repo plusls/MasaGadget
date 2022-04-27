b3e35518f7fb101d9b1d47b5dc9b1c834f1160ab
plulsls <plusls@qq.com>
2022-04-27 20:12:35 +0800

    update modmenu impl

--- src/main/java/com/plusls/MasaGadget/compat/modmenu/WrapperModMenuApiImpl.java
+++ src/main/java/com/plusls/MasaGadget/compat/modmenu/WrapperModMenuApiImpl.java
@@ -1,18 +1,8 @@
 package com.plusls.MasaGadget.compat.modmenu;
 
 import com.plusls.MasaGadget.ModInfo;
-import com.plusls.MasaGadget.gui.GuiConfigs;
-import top.hendrixshen.magiclib.compat.modmenu.ModMenuCompatApi;
 
-public class WrapperModMenuApiImpl implements ModMenuCompatApi {
-    @Override
-    public ConfigScreenFactoryCompat<?> getConfigScreenFactoryCompat() {
-        return (screen) -> {
-            GuiConfigs gui = GuiConfigs.getInstance();
-            gui.setParentGui(screen);
-            return gui;
-        };
-    }
+public class WrapperModMenuApiImpl extends ModMenuApiImpl {
 
     @Override
     public String getModIdCompat() {
