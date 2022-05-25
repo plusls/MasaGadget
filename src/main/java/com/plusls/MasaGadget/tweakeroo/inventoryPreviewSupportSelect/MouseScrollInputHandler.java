20c05b9ae57445ac7aac4328e67e3320e8b035b0
plusls <plusls@qq.com>
2022-05-25 12:58:04 +0800

    fix litematica mouse scroll

--- src/main/java/com/plusls/MasaGadget/tweakeroo/inventoryPreviewSupportSelect/MouseScrollInputHandler.java
+++ src/main/java/com/plusls/MasaGadget/tweakeroo/inventoryPreviewSupportSelect/MouseScrollInputHandler.java
@@ -7,6 +7,9 @@ import fi.dy.masa.malilib.event.InputEventHandler;
 import fi.dy.masa.malilib.hotkeys.IMouseInputHandler;
 import fi.dy.masa.tweakeroo.config.FeatureToggle;
 import fi.dy.masa.tweakeroo.config.Hotkeys;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.Registry;
+import net.minecraft.world.entity.player.Player;
 
 public class MouseScrollInputHandler implements IMouseInputHandler {
     public static void register() {
@@ -16,6 +19,7 @@ public class MouseScrollInputHandler implements IMouseInputHandler {
 
     @Override
     public boolean onMouseScroll(int mouseX, int mouseY, double amount) {
+        Player player = Minecraft.getInstance().player;
         if (ModInfo.isModLoaded(ModInfo.TWEAKEROO_MOD_ID) &&
                 Configs.inventoryPreviewSupportSelect &&
                 FeatureToggle.TWEAK_INVENTORY_PREVIEW.getBooleanValue() &&
@@ -24,6 +28,11 @@ public class MouseScrollInputHandler implements IMouseInputHandler {
                 InventoryOverlayRenderHandler.instance.addSelectedIdx(1);
             } else if (amount > 0) {
                 InventoryOverlayRenderHandler.instance.addSelectedIdx(-1);
+            }
+            if (ModInfo.isModLoaded(ModInfo.LITEMATICA_MOD_ID) &&
+                    fi.dy.masa.litematica.config.Configs.Generic.TOOL_ITEM_ENABLED.getBooleanValue() &&
+                    player != null && Registry.ITEM.getKey(player.getMainHandItem().getItem()).toString().contains(fi.dy.masa.litematica.config.Configs.Generic.TOOL_ITEM.getStringValue())) {
+                return false;
             }
             return true;
         }
