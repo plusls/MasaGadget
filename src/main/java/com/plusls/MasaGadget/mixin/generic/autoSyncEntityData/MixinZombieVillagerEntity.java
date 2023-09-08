8cbcfbcc8ae41e8d81241d337a28555d5a7dd161
violetc <58360096+s-yh-china@users.noreply.github.com>
2023-09-08 16:02:32 +0800

    ZombieVillager convert data sync

--- src/main/java/com/plusls/MasaGadget/mixin/generic/autoSyncEntityData/MixinZombieVillagerEntity.java
+++ src/main/java/com/plusls/MasaGadget/mixin/generic/autoSyncEntityData/MixinZombieVillagerEntity.java
@@ -8,6 +8,7 @@ import net.minecraft.world.entity.monster.Zombie;
 import net.minecraft.world.entity.monster.ZombieVillager;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -15,10 +16,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ZombieVillager.class)
 public abstract class MixinZombieVillagerEntity extends Zombie {
 
-
     public MixinZombieVillagerEntity(EntityType<? extends Zombie> entityType, Level level) {
         super(entityType, level);
     }
+
+    @Shadow
+    public abstract boolean isConverting();
+
+    @Shadow
+    protected abstract int getConversionProgress();
+
+    @Shadow
+    private int villagerConversionTime;
 
     @Inject(method = "handleEntityEvent", at = @At(value = "RETURN"))
     private void syncVillagerData(byte status, CallbackInfo ci) {
@@ -30,6 +39,30 @@ public abstract class MixinZombieVillagerEntity extends Zombie {
         if (status == 16) {
             PcaSyncProtocol.syncEntity(this.getId());
             PcaSyncProtocol.cancelSyncEntity();
+        }
+    }
+
+    @Inject(method = "tick", at = @At(value = "RETURN"))
+    private void syncConvertingData(CallbackInfo ci) {
+        //#if MC > 11904
+        if (this.level().isClientSide() && this.isAlive() && this.isConverting()) {
+        //#elseif MC > 11701
+        //$$ if (this.getLevel().isClientSide() && this.isAlive() && this.isConverting()) {
+        //#else
+        //$$ if (this.level.isClientSide() && this.isAlive() && this.isConverting()) {
+        //#endif
+            int i = this.getConversionProgress();
+            this.villagerConversionTime -= i;
+            if (this.villagerConversionTime <= 0) {
+                // 如果这里为负，应该是没有同步数据
+                if (!Configs.autoSyncEntityData ||
+                        Minecraft.getInstance().hasSingleplayerServer() ||
+                        !PcaSyncProtocol.enable) {
+                    return;
+                }
+                PcaSyncProtocol.syncEntity(this.getId());
+                PcaSyncProtocol.cancelSyncEntity();
+            }
         }
     }
 }
