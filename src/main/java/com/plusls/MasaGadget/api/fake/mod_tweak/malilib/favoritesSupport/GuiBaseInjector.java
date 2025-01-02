f91845d955dcf2514e260e2035a8998e64b40768
Hendrix-Shen <HendrixShen@hendrixshen.top>
2025-01-02 22:42:53 +0800

    Fix compatibility with latest malilib (Related to plusls#144)
    Signed-off-by: Hendrix-Shen <HendrixShen@hendrixshen.top>

---
+++ src/main/java/com/plusls/MasaGadget/api/fake/mod_tweak/malilib/favoritesSupport/GuiBaseInjector.java
@@ -0,0 +1,12 @@
+package com.plusls.MasaGadget.api.fake.mod_tweak.malilib.favoritesSupport;
+
+import org.jetbrains.annotations.ApiStatus;
+
+@ApiStatus.Internal
+public interface GuiBaseInjector {
+    default void masa_gadget_mod$addFastSwitcherWidget() {
+    }
+
+    default void masa_gadget_mod$addFavoritesWidget() {
+    }
+}
