1ebba4dadaa869c1ae8c4504a74b4a61fe5af211
Hendrix-Shen <HendrixShen@hendrixshen.top>
2025-04-27 16:15:54 +0800

    Fix mixin target for `pinyinSouSuo` (MC 1.18+)
    Signed-off-by: Hendrix-Shen <HendrixShen@hendrixshen.top>

--- src/main/java/com/plusls/MasaGadget/mixin/mod_tweak/malilib/pinyinSouSuo/MixinWidgetListBase.java
+++ src/main/java/com/plusls/MasaGadget/mixin/mod_tweak/malilib/pinyinSouSuo/MixinWidgetListBase.java
@@ -10,6 +10,22 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(value = WidgetListBase.class, remap = false)
 public class MixinWidgetListBase {
+    //#if MC > 11701
+    //$$ @WrapOperation(
+    //$$         method = "matchesFilter(Ljava/lang/String;Ljava/lang/String;)Z",
+    //$$         at = @At(
+    //$$                 value = "INVOKE",
+    //$$                 target = "Ljava/lang/String;contains(Ljava/lang/CharSequence;)Z"
+    //$$         )
+    //$$ )
+    //$$ private boolean patchMatchLogic(String instance, CharSequence s, Operation<Boolean> original) {
+    //$$     if (Configs.pinyinSouSuo.getBooleanValue() && PinInHelper.getInstance().contains(instance, s.toString())) {
+    //$$         return true;
+    //$$     }
+    //$$
+    //$$     return original.call(instance, s);
+    //$$ }
+    //#else
     @WrapOperation(
             method = "matchesFilter(Ljava/lang/String;Ljava/lang/String;)Z",
             at = @At(
@@ -26,4 +42,5 @@ public class MixinWidgetListBase {
 
         return ret;
     }
+    //#endif
 }
